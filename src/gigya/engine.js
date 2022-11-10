536de3110dc267443d7c4478b17039f1f5fdb6f9
dinavinter <dina.vinter@sap.com>
2022-11-10 21:00:19 +0200

    homepage

--- src/gigya/engine.js
+++ src/gigya/engine.js
@@ -67,7 +67,7 @@ export function getLanguage() {
 export function initDemoSite() {
     // log('0. Init Demo site');
     // Read configuration file and load it
-    fetch('/us-store-dynidp-rp/config/site.json')
+    fetch('/config/site.json')
         .then((res) => { return res.json(); })
         .then((out) => {
 
