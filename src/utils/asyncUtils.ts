28d1d98d86717a0f761beab38cfe97041181293d
dinavinter <dina.vinter@sap.com>
2022-11-11 16:23:54 +0200

    fix asyncutils

--- src/utils/asyncUtils.ts
+++ src/utils/asyncUtils.ts
@@ -1,5 +1,6 @@
 // import dotenv from "dotenv";
 import axios from "axios";
+import {stateLocalStorage} from "../machines/withLocalStorage";
 
 // dotenv.config();
 
@@ -8,17 +9,14 @@ const httpClient = axios.create({
 });
 
 httpClient.interceptors.request.use((config) => {
-  /* istanbul ignore if */
-  if (
-    process.env.REACT_APP_AUTH0 ||
-    process.env.REACT_APP_OKTA ||
-    process.env.REACT_APP_AWS_COGNITO ||
-    process.env.REACT_APP_GOOGLE
-  ) {
-    const accessToken = localStorage.getItem(process.env.REACT_APP_AUTH_TOKEN_NAME!);
-    // @ts-ignore
-    config.headers["Authorization"] = `Bearer ${accessToken}`;
-  }
+    
+    const accessToken = stateLocalStorage.get()?.context?.token?.access_token;
+    if(accessToken){
+      // @ts-ignore
+      config.headers["Authorization"] = `Bearer ${accessToken}`;
+    }
+  
+  
   return config;
 });
 
