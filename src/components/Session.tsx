efdf99fae4a18af4cd74f9bade5c617236596946
dinavinter <dina.vinter@sap.com>
2022-12-06 06:41:15 +0200

    fixbuild

--- src/components/Session.tsx
+++ src/components/Session.tsx
@@ -1,77 +1,72 @@
-import React, { useState } from "react";
-import gigyaWebSDK from "../gigya/gigyaWebSDK";
- import Avatar from "@mui/material/Avatar";
+import React from "react";
 import makeStyles from '@mui/styles/makeStyles';
-import Button from "@mui/material/Button";
-import { useForm } from "react-hook-form";
-import {Redirect, RouteComponentProps} from "@reach/router";
 import {AuthService} from "../machines/authMachine";
-import {history} from "../utils/historyUtils";
-import {useActor, useSelector} from "@xstate/react";
+import { useSelector} from "@xstate/react";
 import {AnyState} from "xstate";
 import JsonView from "./JsonTreeViewer";
 import {Paper, Typography} from "@mui/material";
 
 const useStyles = makeStyles((theme) => ({
-  paper: {
-    marginTop: theme.spacing(8),
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
-    width: theme.spacing(12),
-    height: theme.spacing(12),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
-  form: {
-    // width: "100%", // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-  },
+    paper: {
+        marginTop: theme.spacing(8),
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+    },
+    avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.secondary.main,
+        width: theme.spacing(12),
+        height: theme.spacing(12),
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2),
+    },
+    form: {
+        // width: "100%", // Fix IE 11 issue.
+        marginTop: theme.spacing(1),
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+    },
 }));
 
- 
+
 export interface SessionProps {
-  authService: AuthService;
+    authService: AuthService;
 
 }
- 
 
-const jwtSelector=(state:AnyState)=>state?.context;
+
+const jwtSelector = (state: AnyState) => state?.context;
+
 function SessionInfo({authService}: SessionProps) {
-  const classes = useStyles();
-  const {idToken, mfaToken} = useSelector(authService, jwtSelector) ||{};
-  
-   
+    const classes = useStyles();
+    const {idToken, mfaToken} = useSelector(authService, jwtSelector) || {};
 
-  return (
-      <Paper className={classes.paper} >
-      <Paper className={classes.paper} >
-        <Typography component="h2" variant="h6" color="primary" gutterBottom>
-          MFA Token
-        </Typography>
-        {mfaToken &&  <JsonView data={mfaToken} />}  
-      </Paper>
-        
-        <Paper className={classes.paper} >
-        <Typography component="h2" variant="h6" color="primary" gutterBottom>
-          Id Token
-        </Typography>
-        {idToken && idToken.details && <JsonView data={idToken.details} />}  
-      </Paper>
-      </Paper>
-  );
+
+    return (
+        <Paper className={classes.paper}>
+            <Paper className={classes.paper}>
+                <Typography component="h2" variant="h6" color="primary" gutterBottom>
+                    MFA Token
+                </Typography>
+                {mfaToken && <JsonView data={mfaToken}/>}
+            </Paper>
+
+            <Paper className={classes.paper}>
+                <Typography component="h2" variant="h6" color="primary" gutterBottom>
+                    Id Token
+                </Typography>
+                {idToken && idToken.details && <JsonView data={idToken.details}/>}
+            </Paper>
+        </Paper>
+    );
 }
-function decodeJwt(token?:string) {
 
-  return token && token.split && JSON.parse(atob(token.split('.')[1]));
+function decodeJwt(token?: string) {
+
+    return token && token.split && JSON.parse(atob(token.split('.')[1]));
 
 }
 
