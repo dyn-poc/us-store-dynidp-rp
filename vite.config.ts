c1cf9ce5a9a3fd50bb707583c21ba15b8d924150
dinavinter <dina.vinter@sap.com>
2022-12-06 07:41:52 +0200

    chunks

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,20 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 import Pages from 'vite-plugin-pages'
+import {splitVendorChunkPlugin} from 'vite'
 
- 
+
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react(), Pages({
         dirs: 'src/pages',
-    })]
+    })],
+    build: {
+        rollupOptions: {
+            output:{minifyInternalExports:true, compact:true},
+            manualChunks: {
+                lodash: ['lodash']
+            }
+        }
+    }
 })
